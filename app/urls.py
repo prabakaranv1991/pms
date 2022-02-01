09ce59bcd9ee872729795e7e62c375d6c65fa88d
Prabakaran V <spksprabu@gmail.com>
2022-02-01 07:41:57 +0530

    Git changes

--- app/urls.py
+++ app/urls.py
@@ -63,5 +63,5 @@ urlpatterns = [
     path('finance/', include(finance)),
     path('other/', include(other)),
     path('admin/', admin.site.urls),
-    url(r'^api/git/$', git_hook, name='git_hook'),
+    url(r'^api/git/$', git_hook, name='git'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
