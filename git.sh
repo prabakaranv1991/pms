6517a7cc8c34a4934d4ef144ad364fbe698febb7
Prabakaran <spksprabu@gmail.com>
2022-03-21 11:42:02 +0530

    Database Backup Upload

--- git.sh
+++ git.sh
@@ -1,6 +1,9 @@
 cd /home/praba_test
 git pull origin master
+echo "Taking Backup"
 docker exec -it praba_test_db_1 pg_dump -U pms pms -Fc -f /home/db_backup/pms.pgd
+echo "Started Git Operations"
 git add .
 git commit -m "Database Backup Upload"
 git push origin master
+echo "Ended Git Operation"
