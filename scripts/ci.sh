91cee5e34e1ec08e7118064f07e2610f1b664668
Prabakaran V <spksprabu@gmail.com>
2022-03-29 13:31:40 +0530

    CI

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,1 +1,4 @@
-cd /home/praba_test; git pull; docker exec -it praba_test_web_1 python3 manage.py makemigrations; docker exec -it praba_test_web_1 python3 manage.py migrate
+cd /home/praba_test
+git pull
+docker exec -it praba_test_web_1 python3 manage.py makemigrations
+docker exec -it praba_test_web_1 python3 manage.py migrate
