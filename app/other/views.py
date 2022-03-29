c7feeac8301937aa12067059fb3248da090e0a59
Prabakaran V <spksprabu@gmail.com>
2022-03-29 13:30:24 +0530

    CI

--- app/other/views.py
+++ app/other/views.py
@@ -40,5 +40,9 @@ class CardUpdate(UpdateView):
 def git(request):
     data = json.loads(request.body.decode('utf-8'))
     if data['head_commit']['message'] != 'Database Backup Upload':
-        os.system('echo sh /home/praba_test/scripts/ci.sh >> /hostpipe &')
+        os.system('echo cd /home/praba_test >> /hostpipe')
+        os.system('echo git pull >> /hostpipe')
+        os.system('echo docker exec -it praba_test_web_1 python3 manage.py makemigrations >> /hostpipe')
+        os.system('echo docker exec -it praba_test_web_1 python3 manage.py migrate >> /hostpipe')
+
     return HttpResponse('Successfully Pulled into the production server...')
--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,4 +1,1 @@
-cd /home/praba_test
-git pull
-docker exec -it praba_test_web_1 python3 manage.py makemigrations
-docker exec -it praba_test_web_1 python3 manage.py migrate
+cd /home/praba_test; git pull; docker exec -it praba_test_web_1 python3 manage.py makemigrations; docker exec -it praba_test_web_1 python3 manage.py migrate
