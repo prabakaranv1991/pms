88dd24910fee129b3c4365378d2df3d23ae823b9
Prabakaran V <spksprabu@gmail.com>
2022-03-29 12:30:15 +0530

    CI

--- git.sh
+++ git.sh
@@ -1,9 +1,10 @@
 cd /home/praba_test
 git pull
-echo "Taking Backup"
+docker-compose down
+docker-compose up -d
 docker exec praba_test_db_1 pg_dump -U pms pms -Fc -f /home/db_backup/pms.pgd
-echo "Started Git Operations"
+docker exec -it praba_test_web_1 python3 manage.py makemigrations
+docker exec -it praba_test_web_1 python3 manage.py migrate
 git add .
 git commit -m "Database Backup Upload"
 git push origin master
-echo "Ended Git Operations"
