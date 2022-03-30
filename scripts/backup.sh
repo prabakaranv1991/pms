ca02e3f66b9d8dba83730c765152c7384e6595f7
Prabakaran <spksprabu@gmail.com>
2022-03-30 09:33:16 +0530

    Database Backup Upload

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,4 @@
-docker exec pms_web pg_dump -U pms pms -Fc -f /home/db_backup/pms.pgd
+docker exec pms_db pg_dump -U pms pms -Fc -f /home/db_backup/pms.pgd
 git add .
 git commit -m "Database Backup Upload"
 git push origin master
