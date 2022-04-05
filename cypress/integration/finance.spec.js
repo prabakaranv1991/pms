8c913ec042e1c65b0187463db65aa8bdcfc7acc8
Prabakaran V <spksprabu@gmail.com>
2022-04-05 22:15:54 +0530

    Cypress Test - 1

--- cypress/integration/finance.spec.js
+++ cypress/integration/finance.spec.js
@@ -4,5 +4,16 @@ describe('Finance', () => {
     })
     it('Change Month', () => {
         cy.visit('finance')
+        var now = new Date();
+        if (now.getMonth() == 11) {
+            var current = new Date(now.getFullYear() + 1, 0, 1);
+        } else {
+            var current = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+        }
+        next_month = current.toLocaleString('default', { month: 'long' }) + "' " +now.getFullYear().toString().substr(-2)
+        cy.get('[name="month"]').select(next_month)
+        cy.url().then(url => {
+            cy.url().should('include', '?month=' + String(next_month.getFullYear()) + '-' + String(d.getMonth()) + '-01' )
+        })
     })
 })
