2debadbb4c2ebf02e7e0c14c502f998315485596
Prabakaran V <spksprabu@gmail.com>
2022-04-05 09:44:33 +0530

    Cypress Test - 23

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 Cypress.Commands.add('login', (username, password) => {
     cy.visit(Cypress.config('baseUrl') + 'login')
     cy.get('input[name=username]').clear().type('admin')
-    cy.get('input[name=password]').clear().type('Praniya_143')
+    cy.get('input[name=password]').clear().type('Praniya143')
     cy.get('button[type=submit]').click()
     cy.url().then(url => {
         cy.url().should('include', '/home')
