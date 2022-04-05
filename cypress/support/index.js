39d4259b7b3235762cfa96c54a9c7831cfda3f3c
Prabakaran V <spksprabu@gmail.com>
2022-04-05 10:05:36 +0530

    Cypress Test Working Test 2

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,20 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+require('dotenv').config()
+
+module.exports = (on, config) => {
+  // copy any needed variables from process.env to config.env
+  config.env.username = process.env.USER_NAME
+
+  // do not forget to return the changed config object!
+  return config
+}
+
+// integration/spec.js
+it('has username to use', () => {
+  expect(Cypress.env('username')).to.be.a('string')
+})
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
