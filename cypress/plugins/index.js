92325567b6db5f9bf8f693c52d228e319870f16e
Prabakaran V <spksprabu@gmail.com>
2022-04-05 20:58:32 +0530

    Cypress Test Working Test 16

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config({ path: '../../../.env' })
+require('dotenv').config()
 
 module.exports = (on, config) => {
   // copy any needed variables from process.env to config.env
-  config.BASEURL = process.env.BASEURL
   console.log(process.env)
+  config.BASEURL = process.env.BASEURL
   // do not forget to return the changed config object!
   return config
 }
