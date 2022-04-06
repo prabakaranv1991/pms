fe93fdd18d99d0af41d66a5708fee49bda9e46c4
Prabakaran V <spksprabu@gmail.com>
2022-04-06 14:31:42 +0530

    UNIT_TEST Secrets

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,7 +18,7 @@ import './commands'
 
 module.exports = (on, config) => {
   // copy any needed variables from process.env to config.env
-  console.log(process.env.USER_NAME)
+  console.log(process.env)
 
   // do not forget to return the changed config object!
   return config
