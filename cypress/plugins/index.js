34e69dda7168bb6b6f7d75f097ec2902a303b287
Prabakaran <spksprabu@gmail.com>
2022-04-19 11:16:49 +0530

    Database Backup Upload

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,5 @@
 module.exports = (on, config) => {
   // copy any needed variables from process.env to config.env
-  console.log(process.env)
 
   // do not forget to return the changed config object!
   return config
